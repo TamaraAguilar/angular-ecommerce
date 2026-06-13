c66afc80bdf37a0a352a158338779f5f3a77e2b6
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-06-13 14:00:16 -0400

    feat: add types

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,7 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Service()
 export class Api {
-  private http = inject(HttpClient);
+  private httpClient = inject(HttpClient);
+
+  get<T>(url: string, options?: any): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
 }
