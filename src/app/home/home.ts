f17693b68297a397516e2c81585d0fb741ea8b03
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-06-13 16:02:32 -0400

    feat: add getProducts

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Products as ProductsService } from '../services/products';
+import { Products } from './../../types';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  private productsService = inject(ProductsService);
+  private readonly baseUrl = 'http://localhost:3000/';
+
+  ngOnInit() {
+    this.productsService
+      .getProducts(`${this.baseUrl}clothes`, { page: 0, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
+}
--- src/app/services/products.ts
+++ src/app/services/products.ts
@@ -1,13 +1,13 @@
 import { inject, Service } from '@angular/core';
 import { Api } from './api';
 import { Observable } from 'rxjs';
-import { PaginationParams } from '../../types';
+import { PaginationParams, Products as ProductsResponse } from '../../types';
 
 @Service()
 export class Products {
   private apiService = inject(Api);
 
-  getProducts = (url: string, params: PaginationParams): Observable<Products> => {
+  getProducts = (url: string, params: PaginationParams): Observable<ProductsResponse> => {
     return this.apiService.get(url, {
       params,
       responseType: 'json',
